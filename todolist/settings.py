d1a357f1495690b1fa90ae6f2efb88fedaf39ce0
Yeonjae37 <jyj030307@gmail.com>
2024-06-18 13:25:31 +0900

    chore: CORS 에러 방지 설정 추가

--- todolist/settings.py
+++ todolist/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'todoApp',
     'userApp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'todolist.urls'
 
